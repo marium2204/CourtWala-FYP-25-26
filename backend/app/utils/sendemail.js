a93fdcc02fa33930cf0af0b4d7ccbac13f507efc
Marium Haris <mariumharis22@gmail.com>
2026-02-03 20:22:30 +0500

    fix nodemailer env config

--- backend/app/utils/sendemail.js
+++ backend/app/utils/sendemail.js
@@ -3,12 +3,16 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: process.env.EMAIL_SECURE === 'true', // false for 587
+  secure: process.env.EMAIL_SECURE === 'true',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  connectionTimeout: 10_000, // 10 seconds
+  greetingTimeout: 10_000,
+  socketTimeout: 10_000,
 });
+
 
 const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
